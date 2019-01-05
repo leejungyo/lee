efac5f1275cc918c2a6a79e71767a7eb0967b595
leejungyo <46391709+leejungyo@users.noreply.github.com>
2019-01-05 14:49:56 +0900

    Add files via upload

--- day5/src/window2/Pizza.java
+++ day5/src/window2/Pizza.java
@@ -7,8 +7,10 @@ import java.awt.event.ActionListener;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
+import java.awt.Color;
 
 public class Pizza {
 	JFrame f; 
@@ -21,30 +23,47 @@ public class Pizza {
     int bulgoki= 0;
     
     JTextArea list;
+    private JLabel lblNewLabel;
+    private JButton btnNewButton;
     
     public Pizza() {
     	
 		f = new JFrame();
-		f.setSize(350, 200);
+		f.setSize(350, 300);
 		
 		top = new JLabel("<<<자바 피자에 오신 것을 환영합니다.반갑습니다>>>");
 		count = new JLabel("개수");
 		b1 = new JButton("콤보피자");
+		b1.setBackground(Color.YELLOW);
 		b2 = new JButton("포테이토피자");
 		b3 = new JButton("불고기피자");
 		total = new JTextField(20);
 		
 		FlowLayout flow = new FlowLayout();
-	    f.setLayout(flow);
+	    f.getContentPane().setLayout(flow);
 	    list = new JTextArea(3, 30);
 	    
-	    f.add(top);
-	    f.add(b1);
-	    f.add(b2);
-	    f.add(b3);
-	    f.add(count);
-	    f.add(total);
-	    f.add(list);
+	    f.getContentPane().add(top);
+	    f.getContentPane().add(b1);
+	    f.getContentPane().add(b2);
+	    f.getContentPane().add(b3);
+	    f.getContentPane().add(count);
+	    f.getContentPane().add(total);
+	    f.getContentPane().add(list);
+	    
+	    btnNewButton = new JButton("\uB204\uB974\uC138\uC694");
+	    btnNewButton.addActionListener(new ActionListener() {
+	    	public void actionPerformed(ActionEvent arg0) {
+	    	JOptionPane.showMessageDialog(null, "나를 정말 누르셨군요");
+	    	
+	    	}
+	    });
+	    
+	    lblNewLabel = new JLabel("\uCD1D\uACC4");
+	    lblNewLabel.setBackground(Color.RED);
+	    lblNewLabel.setForeground(Color.RED);
+	    f.getContentPane().add(lblNewLabel);
+	    f.getContentPane().add(btnNewButton);
 	    
 	    b1.addActionListener(new ActionListener() {
 			@Override
